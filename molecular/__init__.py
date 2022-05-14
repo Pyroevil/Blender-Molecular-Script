2f932a08d5b48e9ddfdf766350bc1ccfd17ab8aa
scorpion81 <martin.felke@googlemail.com>
2022-05-14 16:36:01 +0200

    version bump

--- molecular/__init__.py
+++ molecular/__init__.py
@@ -22,8 +22,8 @@ bl_info = {
         "Jean-Francois Gallant (PyroEvil), "
         "Pavel_Blend, "
         "Martin Felke (scorpion81)",
-    "version": (1, 1, 2),
-    "blender": (2, 80, 0),
+    "version": (1, 1, 3),
+    "blender": (3, 0, 0),
     "location": "Properties editor > Particles Tabs",
     "description":
         "Addon for calculating collisions "
