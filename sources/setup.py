1f6400d0b83b92c516df397cf8ecdf6b1f19a92b
scorpion81 <martin.felke@googlemail.com>
2022-05-14 18:42:43 +0200

    build a "fat" binary for mac os

--- sources/setup.py
+++ sources/setup.py
@@ -7,6 +7,7 @@ import Cython.Compiler.Options
 os_name = platform.architecture()[1]
 Cython.Compiler.Options.annotate = True
 module_name = 'core'
+is_linux = platform.architecture()[1] == "ELF" or platform.system() == "Linux"
 
 if os_name == "WindowsPE":
     ext_modules = [Extension(
@@ -15,12 +16,23 @@ if os_name == "WindowsPE":
         extra_compile_args=['/Ox','/openmp','/GT','/arch:SSE2','/fp:fast'],
         cython_directives={'language_level' : "3"}
     )]
-else:
-    ext_modules = [Extension(
+elif is_linux:
+     ext_modules = [Extension(
         module_name,
         ['core' + '.pyx'],
         extra_compile_args=['-O3','-msse4.2','-ffast-math','-fno-builtin'],
         extra_link_args=['-lm'],
+        cython_directives={'language_level' : "3"}
+    )]
+else:
+    # under mac OS, with the advent of the M1 ARM chips, its necessary to build an universal intel/arm binary.
+    # this is done by passing -arch arm64 -arch x86_64 from here to clang, the default mac OS compiler.
+
+    ext_modules = [Extension(
+        module_name,
+        ['core' + '.pyx'],
+        extra_compile_args=['-O3','-msse4.2','-ffast-math','-fno-builtin','-arch','arm64','-arch','x86_64'],
+        extra_link_args=['-lm','-arch','arm64','-arch','x86_64'],
         cython_directives={'language_level' : "3"}
     )]
 
